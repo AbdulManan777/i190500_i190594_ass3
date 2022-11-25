7123c33cd5c02c0fc2aac0b659c9c8d77bbf62e0
umarmansoor001 <i190594@nu.edu.pk>
2022-11-25 18:54:10 +0500

    voice recorder ui done

--- app/src/main/java/com/ass3/i190500_i190594/Contacts_Activity.java
+++ app/src/main/java/com/ass3/i190500_i190594/Contacts_Activity.java
@@ -1,50 +1,78 @@
-package com.ass3.i190500_i190594;
+        package com.ass3.i190500_i190594;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+        import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.drawerlayout.widget.DrawerLayout;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+        import android.content.Intent;
+        import android.media.Image;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.Gravity;
+        import android.view.View;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+        import com.android.volley.AuthFailureError;
+        import com.android.volley.Request;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.StringRequest;
+        import com.android.volley.toolbox.Volley;
+        import com.bumptech.glide.Glide;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 public class Contacts_Activity extends AppCompatActivity {
 
     RecyclerView rv;
-   // ImageView add;
+    // ImageView add;
     MyAdapter adapter;
     List<MyContact> ls;
+    ImageView profile, profile_dp;
+    TextView userName;
+    String url2;
+    DrawerLayout dwr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
         rv=findViewById(R.id.msgs_list_RV);
+        profile=findViewById(R.id.profielpicture);
+        dwr = findViewById(R.id.drwr);
+        profile_dp=findViewById(R.id.profile_dp);
+        userName=findViewById(R.id.user_name);
 
 
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(dwr.isDrawerOpen(Gravity.LEFT))
+                    dwr.closeDrawer(Gravity.LEFT);
+                else
+                    dwr.openDrawer(Gravity.LEFT);
+            }
+        });
 
 
         String username=getIntent().getStringExtra("Username");
         String password=getIntent().getStringExtra("Password");
+        // setting user name on profile xml file
+        userName.setText(username);
+
+        // Recycler view for keeping chats rows
         RecyclerView.LayoutManager lm=new LinearLayoutManager(Contacts_Activity.this);
         ls=new ArrayList<>();
         adapter=new MyAdapter(Contacts_Activity.this,ls,username);
@@ -55,11 +83,86 @@ public class Contacts_Activity extends AppCompatActivity {
         Log.i("Password",password);
 
 
+        //Getting Current User Dp
+        StringRequest request2=new StringRequest(Request.Method.POST, "http://"+IP_server.getIP_server()+"/smdass3/getImageOnly.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj=new JSONObject(response);
+
+                            if(obj.getInt("code")==1)
+                            {
+                                JSONArray contacts=obj.getJSONArray("pictures");
+
+                                for (int i=0; i<contacts.length();i++)
+                                {
+
+                                    JSONObject contact=contacts.getJSONObject(i);
+                                    //String usern=contact.getString("Username");
+                                    String image=contact.getString("image");
+                                    url2="http://"+IP_server.getIP_server()+"/smdass3/images/"+image;
+
+                                    //MyContact m=new MyContact(usern,url);
+                                    //ls.add(m);
+                                    //adapter.notifyDataSetChanged();
+                                }
+
+                            }
+                            else{
+                                Toast.makeText(Contacts_Activity.this,obj.get("msg").toString(),Toast.LENGTH_LONG).show();
+                            }
+                            Glide.with(Contacts_Activity.this).load(url2).into(profile);
+
+                        } catch (JSONException e) {
+                            Toast.makeText(Contacts_Activity.this,response,Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Contacts_Activity.this,"Error Connecting Server",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+        )
+        {
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params=new HashMap<>();
+                Log.i("Username",username);
+                Log.i("Password",password);
+                params.put("Username",username);
+                // params.put("Password",password);
+                // params.put("PhoneNum",phonenum.getText().toString());
+
+                return params;
+            }
+        };
+
+
+        RequestQueue queue2= Volley.newRequestQueue(Contacts_Activity.this);
+        queue2.add(request2);
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+        //Getting COntacts with thier Profile pics
         StringRequest request=new StringRequest(Request.Method.POST, "http://"+IP_server.getIP_server()+"/smdass3/getUserImg.php",
                 new Response.Listener<String>() {
                     @Override
@@ -101,7 +204,7 @@ public class Contacts_Activity extends AppCompatActivity {
                     }
                 }
 
-                )
+        )
         {
 
             @Nullable
@@ -111,7 +214,7 @@ public class Contacts_Activity extends AppCompatActivity {
                 Log.i("Username",username);
                 Log.i("Password",password);
                 params.put("Username",username);
-                params.put("Password",password);
+                // params.put("Password",password);
                 // params.put("PhoneNum",phonenum.getText().toString());
 
                 return params;
@@ -122,4 +225,6 @@ public class Contacts_Activity extends AppCompatActivity {
         RequestQueue queue= Volley.newRequestQueue(Contacts_Activity.this);
         queue.add(request);
     }
+
+
 }
---
+++ app/src/main/java/com/ass3/i190500_i190594/voice_recorder.java
@@ -0,0 +1,14 @@
+package com.ass3.i190500_i190594;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class voice_recorder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recorder);
+    }
+}

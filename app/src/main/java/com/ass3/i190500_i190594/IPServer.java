23a37d6ae7a4aeee737c01ed684f720637d8cdde
umarmansoor001 <i190594@nu.edu.pk>
2022-11-26 17:34:04 +0500

    Chat Application done

--- app/src/main/java/com/ass3/i190500_i190594/IPServer.java
+++ app/src/main/java/com/ass3/i190500_i190594/IPServer.java
@@ -1,7 +1,7 @@
 package com.ass3.i190500_i190594;
 
 public class IPServer {
-    private static String IP_server = "192.168.10.5";
+    private static String IP_server = "192.168.10.5"; // Manan's IP address
     public static String getIP_server(){
         return IP_server;
     }
